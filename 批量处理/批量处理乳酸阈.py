34100e980eb5cff86bed6665482e17534c3d9120
路畅达 <luchangda_njfu.edu.cn>
2023-11-22 09:44:36 +0800

    批量处理

--- 批量处理/批量处理乳酸阈.py
+++ 批量处理/批量处理乳酸阈.py
@@ -8,7 +8,7 @@ plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 # df = pd.read_excel(r"C:\Users\Administrator\Desktop\hhh.xls", usecols=[0, 1])
 
 # 01 陈明健---
-data01={
+data01 = {
     "intensity": [6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6, 14.8, 16, 17.2],
     "lactate": [2.07, 2.38, 2.41, 2.61, 2.46, 3.30, 3.92, 3.90,3.91, 4.06]
 }
@@ -21,21 +21,13 @@ data02={
     "intensity": [4, 5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6],
     "lactate": [1.97,1.18,1.73,2.00,2.44,3.27,5.00,8.16 ,12.28]
 }
-hr_data02={
-    "heart": [112
-,115
-,133
-,154
-,173
-,181
-,194
-,197
-,203
+hr_data02 = {
+    "heart": [112,115,133,154,173,181,194,197,203
 ],
     "intensity": [4, 5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6]
 }
 # 03---
-data03={
+data03 = {
     "intensity": [4, 5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6],
     "lactate": [ 1.11, 1.16, 1.42, 1.83, 2.22, 2.93, 4.76, 6.68]
 }
@@ -61,13 +53,22 @@ hr_data05={
     "heart": [105, 106, 134, 150, 158, 168, 178, 184, 192],
     "intensity": [4, 5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6]
 }
+# 06---
+data06={
+    "intensity": [4,5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4,13.6],
+    "lactate": [ 1.2, 1.6, 1.6, 2.4, 4.3, 2.6, 3.8, 6.0, 15.5]
+}
+hr_data06={
+    "heart": [99,102,107,133,149,163,172,180,188],
+    "intensity": [4, 5.2, 6.4, 7.6, 8.8, 10, 11.2, 12.4, 13.6]
+}
 
 # -------------------------------------------
-data = data02
-hr_data = hr_data02
+data = data06
+hr_data = hr_data06
 
 df = pd.DataFrame(data)
-HR_df = pd.DataFrame(hr_data )
+HR_df = pd.DataFrame(hr_data)
 
 x = df.iloc[:, 0].values
 y = df.iloc[:, 1].values
@@ -111,14 +112,20 @@ ax1.plot(hx_fit, hy_fit, 'r', alpha=0)
 
 # 绘制心率拟合曲线
 h_fit = hp(x_fit)
-
 ax2 = ax1.twinx()
 ax2.plot(hx_fit, h_fit, 'lightcoral', label='心率曲线')
 
 
-print(h_fit)
 # 找到第一个数据点和最后一个数据点的坐标 1
-first_point = (x[1], y[1])
+# DmaxMod找到两点间插值大于0.4的点连线
+for i in range(len(y)-1):
+    point_distance = y[i+1] - y[i]
+    if (point_distance >= 0.4):
+        point_index = i
+        print(point_distance, i)
+        break
+
+first_point = (x[point_index], y[point_index])
 last_point = (x[-1], y[-1])
 
 # 绘制直线连接第一个点和最后一个点
@@ -127,19 +134,21 @@ ax1.plot([first_point[0], last_point[0]], [first_point[1], last_point[1]], 'g--'
 # 计算拟合曲线上每个点到直线的距离
 distances = []
 
+
 for i in range(len(x_fit)):
     # 计算点到直线的距离公式：|Ax + By + C| / sqrt(A^2 + B^2)
 
     # 这里我遍历的是拟合的曲线到第一个和最后一个点连线的距离 2
     distance = ((last_point[0] - first_point[0]) * (first_point[1] - y_fit[i]) - (first_point[0] - x_fit[i]) * (
-                last_point[1] - first_point[1])) / np.sqrt((last_point[0] - first_point[0]) ** 2 + (last_point[1] - first_point[1]) ** 2)
+                last_point[1] - first_point[1]))/np.sqrt((last_point[0] - first_point[0]) ** 2 + (last_point[1] - first_point[1]) ** 2)
     distances.append(distance)
 
 # 在这里可以打印一下看看每个点到直线的距离 3
 print("距离:", distances)
 
 # 找到距离直线最远的点的索引
-index_max_dist = np.argmax(distances)
+# index_max_dist = np.argmax(distances)
+index_max_dist = distances.index(max(distances))
 
 # 获取距离直线最远的点的坐标
 x_max_dist = x_fit[index_max_dist]
@@ -159,7 +168,6 @@ ax1.annotate(f'乳酸阈值:{y_max_dist:.2f} mmol/L', xy=(x_max_dist, y_max_dist
 #              arrowprops=dict(arrowstyle='->', color='black'))
 ax2.annotate(f'直测法测量  乳酸阈心率:{y_hr:.0f} bpm, 乳酸阈跑速:{x_max_dist:.2f}km/h', xy=(x_max_dist, y_hr), xytext=(x_max_dist - 4, y_hr+7),
              arrowprops=dict(arrowstyle='->', color='black'))
-
 
 ax1.axhline(y=1.15, color='black', linestyle='--', label="基线(静息血乳酸)")
 ax1.axhline(y=3.5, color='orange', linestyle='--', label="3.5 mmol/L乳酸阈")
